1713ef20d329137742038a2ccaeb4491895489fc
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-21 17:07:36 +0100

    Agregar soporte para Tailwind CSS y actualizar dependencias en package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 
+import tailwindcss from '@tailwindcss/vite';
+
+
 export default defineConfig({
-    integrations: [react()],
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });

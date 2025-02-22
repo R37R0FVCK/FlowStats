73e708789c4d12f5b8d698d2d03431d455d32dbc
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-22 20:04:11 +0100

    Reemplaza la función de búsqueda por la implementación específica para cantantes en el servidor

--- servidorbbdd.js
+++ servidorbbdd.js
@@ -1,12 +1,10 @@
-// iniciar server
-
 import express from 'express';
-import busqueda from './src/api/funcionbusqueda.js';
+import busquedacantantes from './src/api/funcionbusquedacantantes.js';
 
 const app = express();
 const puertoacceso = 3000;
 
-app.get('/api/search', busqueda);
+app.get('/api/search', busquedacantantes);
 
 app.listen(puertoacceso, () => {
     console.log(`Servidor en http://localhost:${puertoacceso}`);

178117a9e5d13b37fec38b40b3106785b101e279
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-23 00:25:27 +0100

    Se añade configuración de CORS al servidor

--- servidorbbdd.js
+++ servidorbbdd.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import busquedacantantes from './src/api/funcionbusquedacantantes.js';
 
 const app = express();
 const puertoacceso = 3000;
+
+// Configurar CORS
+app.use(cors());
 
 app.get('/api/search', busquedacantantes);
 

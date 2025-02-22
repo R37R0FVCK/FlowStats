d4a1ad272567fd4a1e7a849c5cd9ad9dd4be58c6
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-22 10:59:18 +0100

    Se agrega Astro DB  y dependencias para el manejo y control de las BBDD

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,11 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import db from '@astrojs/db';
+
+
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), db()],
 
   vite: {
     plugins: [tailwindcss()],

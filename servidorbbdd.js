a9760ac173492f0bd9e1b92752297560dadf68ac
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-22 18:17:54 +0100

    Refactor el servidor para utilizar importaciones ES6 y eliminar el endpoint de prueba de conexión a la base de datos

--- servidorbbdd.js
+++ servidorbbdd.js
@@ -1,24 +1,12 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const busqueda = require('./src/api/funcionbusqueda');
+// iniciar server
+
+import express from 'express';
+import busqueda from './src/api/funcionbusqueda.js';
 
 const app = express();
 const puertoacceso = 3000;
-const prisma = new PrismaClient();
 
 app.get('/api/search', busqueda);
-
-// Endpoint de prueba para verificar la conexión a la base de datos
-app.get('/api/test-db', async (req, res) => {
-    try {
-        await prisma.$connect();
-        res.status(200).send('Conexión a la base de datos exitosa');
-    } catch (error) {
-        res.status(500).send('Error al conectar a la base de datos: ' + error.message);
-    } finally {
-        await prisma.$disconnect();
-    }
-});
 
 app.listen(puertoacceso, () => {
     console.log(`Servidor en http://localhost:${puertoacceso}`);

0df506ccfd31e30f38e94cb766ab81c6821bd685
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-23 19:36:58 +0100

    Se añade la configuración de la tabla Cantante en la base de datos y se exportan las configuraciones de User y Cantante.

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,5 @@
 import { column, defineDb } from 'astro:db';
 
-
 const User = {
   columns: {
     nombre: column.text({ primaryKey: true }),
@@ -11,13 +10,31 @@ const User = {
     fecha_nacimiento: column.text(),
     tipo_usuario: column.text(),
     telefono: column.text({ unique: true }),
-
   }
-}
-// https://astro.build/db/config
+};
+
+const Cantante = {
+  columns: {
+    cod_art: column.number({ primaryKey: true }),
+    nom_art: column.text(),
+    apel_art: column.text(),
+    nombre_real_art: column.text(),
+    edad_art: column.number(),
+    oyentes_art: column.number(),
+    nacionalidad_art: column.text(),
+    anio_comienzo_art: column.number(),
+    cod_manager: column.number(),
+    cod_sello: column.number(),
+  }
+};
+
+
+// Exportar la configuración de la base de datos
 export default defineDb({
   tables: {
-
     User,
+    Cantante,
   }
 });
+
+export { User, Cantante };

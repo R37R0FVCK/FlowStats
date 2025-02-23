2c93adf0c0e6d13968e20729aee45ccddfa671c3
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-23 19:38:22 +0100

    Se simplifica la función de búsqueda de cantantes recuperando todos los artistas.

--- src/api/funcionbusquedacantantes.js
+++ src/api/funcionbusquedacantantes.js
@@ -1,20 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-const busquedacantantes = async (peticion, respuesta) => {
-    const { cons } = peticion.query;
-    if (!cons) {
-        return respuesta.status(400).json({ error: "Los datos introducidos no han podido ser procesados" });
-    }
-
+const busquedacantantes = async (respuesta) => {
     try {
-        const busqueda = await prisma.artista.findMany({
-            where: {
-                nom_art: {
-                    contains: cons,
-                },
-            },
-        });
+        const busqueda = await prisma.artista.findMany();
         respuesta.json(busqueda);
         console.log(busqueda);
     } catch (error) {

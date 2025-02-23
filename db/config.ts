9935fc3a447eefd6b135359f9cf9d5bd5a0c7c4a
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-23 16:05:32 +0100

    Se añade la configuración de la tabla User en la base de datos con sus columnas correspondientes.

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,23 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb } from 'astro:db';
 
+
+const User = {
+  columns: {
+    nombre: column.text({ primaryKey: true }),
+    contraseña: column.text({ unique: true }),
+    apellidos: column.text(),
+    correo: column.text({ unique: true }),
+    direccion: column.text(),
+    fecha_nacimiento: column.text(),
+    tipo_usuario: column.text(),
+    telefono: column.text({ unique: true }),
+
+  }
+}
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+
+    User,
+  }
 });

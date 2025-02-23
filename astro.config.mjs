6d0e7db27f5754119a8b718e081e6e334215bbc6
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-23 16:07:55 +0100

    Se simplifica la configuración de Astro eliminando líneas innecesarias en la integración de la base de datos.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+
+
 import tailwindcss from '@tailwindcss/vite';
+
+
 import db from '@astrojs/db';
 
+
 export default defineConfig({
-  integrations: [
-    react(),
-    db()
-  ],
+  integrations: [react(), db()],
 
   vite: {
     plugins: [tailwindcss()],
   },
-
-  db: {
-    connectionString: 'mysql://root@localhost:3306/musica2'
-  }
 });

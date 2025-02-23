5c489115740d45f5a780d9c96c37931fd5ca65ae
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-02-23 23:27:46 +0100

    Se mejora la función de búsqueda de cantantes para manejar parámetros de consulta y se optimiza el manejo de errores.

--- src/api/funcionbusquedacantantes.js
+++ src/api/funcionbusquedacantantes.js
@@ -1,13 +1,28 @@
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
-const busquedacantantes = async (respuesta) => {
+const busquedacantantes = async (req, res) => {
+    const { cons } = req.query;
+
+    if (!cons) {
+        return res.status(400).json({ error: 'Falta el parámetro de consulta' });
+    }
+
     try {
-        const busqueda = await prisma.artista.findMany();
-        respuesta.json(busqueda);
-        console.log(busqueda);
+        const resultados = await prisma.artista.findMany({
+            where: {
+                nom_art: {
+                    contains: cons,
+                    mode: 'insensitive',
+                },
+            },
+        });
+        res.status(200).json(resultados);
     } catch (error) {
-        respuesta.status(500).json({ error: 'Error al realizar la búsqueda: ' + error.message });
+        res.status(500).json({ error: 'Error al realizar la búsqueda: ' + error.message });
+    } finally {
+        await prisma.$disconnect();
     }
 };
 

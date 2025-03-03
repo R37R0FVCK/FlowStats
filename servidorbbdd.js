ac94718d1dc2b7b677d7850f981d3d82edbd1e12
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-03-03 21:39:22 +0100

    Se eliminan rutas innecesarias

--- servidorbbdd.js
+++ servidorbbdd.js
@@ -4,29 +4,21 @@ import bodyParser from 'body-parser';
 import busquedacantantes from './src/api/funcionbusquedacantantes.js';
 import ObtenerAllCantantes from './src/api/ObtenerAllCantantes.js';
 import Añadircantantes from './src/scripts/añadircantantesAstrodb.js';
-import insertarUsuario from './src/api/insertarUsuario.js';
 
 const api = express();
 const puertoacceso = 3000;
 
-// Configurar CORS
 api.use(cors());
 
-// Middleware para parsear JSON y URL-encoded
 api.use(express.json());
 api.use(bodyParser.urlencoded({ extended: true }));
 
-// Ruta para buscar cantantes por nombre
 api.get('/api/search', busquedacantantes);
 
-// Ruta para obtener todos los cantantes
 api.get('/api/obtenercantantes', ObtenerAllCantantes);
 
-// Ruta para añadir cantantes desde una URL
 api.post('/api/anadircantantes', Añadircantantes);
 
-// Ruta para insertar un usuario
-api.post('/api/insertarUsuario', insertarUsuario);
 
 api.listen(puertoacceso, () => {
     console.log(`Servidor corriendo en http://localhost:${puertoacceso}`);

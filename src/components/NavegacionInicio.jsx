9babe077f64b2a2cc4731f56d4cbe6055d51eff8
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-03-05 01:59:26 +0100

    Se añaden botones de acceso y registro

--- src/components/NavegacionInicio.jsx
+++ src/components/NavegacionInicio.jsx
@@ -24,34 +24,46 @@ const Menupantallamin = () => {
             ✖
           </button>
           <button
-            className="boton-oscuro text-black font-bold px-4 py-2 my-2"
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
             onClick={() => window.location.href = '/'}
           >
             Inicio
           </button>
           <button
-            className="boton-oscuro text-black font-bold px-4 py-2 my-2"
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
             onClick={() => window.location.href = '/Noticias'}
           >
             Noticias
           </button>
           <button
-            className="boton-oscuro text-black font-bold px-4 py-2 my-2"
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
             onClick={() => window.location.href = '/Estadisticas'}
           >
             Estadisticas
           </button>
           <button
-            className="boton-oscuro text-black font font-bold px-4 py-2 my-2"
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
             onClick={() => window.location.href = '/Explorar'}
           >
             Explorar
           </button>
           <button
-            className="boton-oscuro text-black font-bold px-4 py-2 my-2"
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
             onClick={() => window.location.href = '/Contacto'}
           >
             Contacto
+          </button>
+          <button
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
+            onClick={() => window.location.href = '/Acceder'}
+          >
+            Acceder
+          </button>
+          <button
+            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
+            onClick={() => window.location.href = '/Registro'}
+          >
+            Registrarse
           </button>
         </div>
       )}

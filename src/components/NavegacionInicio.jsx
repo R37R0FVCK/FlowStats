2be8a12625c9ce7b8ba7c1e5fa900735640dd6f5
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-03-06 23:25:41 +0100

    Se añade el nombre de usuario activo al menú de navegación

--- src/components/NavegacionInicio.jsx
+++ src/components/NavegacionInicio.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Menupantallamin = ({ accedioacuenta }) => {
+const Menupantallamin = ({ accedioacuenta, nombreusuarioactivo }) => {
   const [abrirmenu, estadomenu] = useState(false);
 
   const activarmenu = () => {
@@ -9,10 +9,7 @@ const Menupantallamin = ({ accedioacuenta }) => {
 
   return (
     <div className="relative md:hidden">
-      <button
-        className="text-white px-4 py-2"
-        onClick={activarmenu}
-      >
+      <button className="text-white px-4 py-2" onClick={activarmenu}>
         <img
           className='w-10 h-10'
           src="https://imagedelivery.net/DX6aCRCKOB_y-l4MZVDT9w/3ceb3a02-b18c-4074-282b-999af8172b00/public"
@@ -21,61 +18,34 @@ const Menupantallamin = ({ accedioacuenta }) => {
       </button>
       {abrirmenu && (
         <div className="fixed inset-0 bg-orange-400 flex flex-col items-center justify-center z-50">
-          <button
-            className="text-white px-4 py-2 self-end"
-            onClick={activarmenu}
-          >
+          <button className="text-white px-4 py-2 self-end" onClick={activarmenu}>
             ✖
           </button>
-          <button
-            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-            onClick={() => window.location.href = '/'}
-          >
+          <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/'}>
             Inicio
           </button>
-          <button
-            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-            onClick={() => window.location.href = '/Noticias'}
-          >
+          <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Noticias'}>
             Noticias
           </button>
-          <button
-            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-            onClick={() => window.location.href = '/Estadisticas'}
-          >
+          <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Estadisticas'}>
             Estadisticas
           </button>
-          <button
-            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-            onClick={() => window.location.href = '/Explorar'}
-          >
+          <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Explorar'}>
             Explorar
           </button>
-          <button
-            className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-            onClick={() => window.location.href = '/Contacto'}
-          >
+          <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Contacto'}>
             Contacto
           </button>
           {accedioacuenta ? (
-            <button
-              className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-              onClick={() => window.location.href = '/MiCuenta'}
-            >
-              Mi Cuenta
+            <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Micuenta'}>
+              Hola {nombreusuarioactivo}
             </button>
           ) : (
             <>
-              <button
-                className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-                onClick={() => window.location.href = '/Acceder'}
-              >
+              <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Acceder'}>
                 Acceder
               </button>
-              <button
-                className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2"
-                onClick={() => window.location.href = '/Registro'}
-              >
+              <button className="boton-oscuro text-black font-bold text-lg px-6 py-3 my-2" onClick={() => window.location.href = '/Registro'}>
                 Registrarse
               </button>
             </>

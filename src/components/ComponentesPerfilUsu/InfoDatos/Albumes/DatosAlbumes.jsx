12ab71ed8c526e3f1ad1f14b1a2b5d2ead56e1eb
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-03-16 23:36:19 +0100

    Agregar componente DatosAlbumes para mostrar álbumes gustados y permitir la selección de álbumes

---
+++ src/components/ComponentesPerfilUsu/InfoDatos/Albumes/DatosAlbumes.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import TarjetaInfoAlbum from './TarjetaInfoAlbum.jsx';
+
+const DatosAlbumes = ({ albumes_gustados }) => {
+    const [Ver_info_Album, asignarAlbum] = useState(null);
+
+    const abrirTarjetaAlbum = (album) => {
+        asignarAlbum(album);
+    };
+
+    const cerrarTarjetaAlbum = () => {
+        asignarAlbum(null);
+    };
+
+    return (
+        <div>
+            <ul className="flex flex-col items-center">
+                {albumes_gustados.map((album) => (
+                    <li
+                        key={album.id_album}
+                        className="mb-1 flex items-center space-x-2 group"
+                        onClick={() => abrirTarjetaAlbum(album)}
+                    >
+                        <div className="relative flex-shrink-0">
+                            <img
+                                src={album.imagen_album}
+                                alt={album.nombre_album}
+                                className="h-25 w-25 object-cover rounded-lg"
+                            />
+                            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 rounded-lg" />
+                            <span className="absolute inset-0 flex items-center justify-center text-white font-bold opacity-0 group-hover:opacity-100">
+                                {album.nombre_album}
+                            </span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
+            {Ver_info_Album && (
+                <TarjetaInfoAlbum
+                    album={Ver_info_Album}
+                    onClose={cerrarTarjetaAlbum}
+                />
+            )}
+        </div>
+    );
+};
+
+export default DatosAlbumes;

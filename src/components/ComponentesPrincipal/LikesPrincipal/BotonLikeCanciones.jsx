519e3646f4feec25f4d161f1d6b15f223768b518
Angel Prada Lopez <angelpradakuai@gmail.com>
2025-03-16 15:25:27 +0100

    Agregar componentes BotonLikeArtistas, BotonLikeCanciones y BotonLikeAlbum para gestionar likes en artistas, canciones y álbumes

---
+++ src/components/ComponentesPrincipal/LikesPrincipal/BotonLikeCanciones.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react';
+
+const BotonLikeCanciones = ({ cod_cancion, cod_usu, estado_megusta }) => {
+    const [megusta, asignar_megusta] = useState(() => estado_megusta(cod_cancion));
+
+    useEffect(() => {
+        asignar_megusta(estado_megusta(cod_cancion));
+    }, [cod_cancion, estado_megusta]);
+
+    const canciones_gustadas = async () => {
+        try {
+            const respuesta_envio = await fetch('http://localhost:4321', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ cod_cancion, cod_usu }),
+            });
+
+            if (!respuesta_envio.ok) {
+                throw new Error('Error al enviar la solicitud');
+            }
+
+            const datos = await respuesta_envio.json();
+            asignar_megusta(!megusta);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const cambio_estado = () => {
+        canciones_gustadas();
+    };
+
+    return (
+        <div title="Like" className="heart-container" onClick={cambio_estado}>
+            <input id="Give-It-An-Id" className="checkbox" type="checkbox" checked={megusta} readOnly />
+            <div className="svg-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="svg-outline"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"
+                    >
+                    </path>
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="svg-filled"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z"
+                    >
+                    </path>
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="100"
+                    width="100"
+                    className="svg-celebrate"
+                >
+                    <polygon points="10,10 20,20"></polygon>
+                    <polygon points="10,50 20,50"></polygon>
+                    <polygon points="20,80 30,70"></polygon>
+                    <polygon points="90,10 80,20"></polygon>
+                    <polygon points="90,50 80,50"></polygon>
+                    <polygon points="80,80 70,70"></polygon>
+                </svg>
+            </div>
+        </div>
+    );
+};
+
+export default BotonLikeCanciones;
